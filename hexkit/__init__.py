79d56f800e2ea6f7e6c30ab5a16f251b9a43b41b
KerstenBreuer <kersten-breuer@outlook.com>
2022-04-01 12:43:18 +0000

    adapt version

--- hexkit/__init__.py
+++ hexkit/__init__.py
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A library that contains the basic chassis functionality used in services of GHGA"""
+"""A Toolkit for Building Microservices using the Hexagonal Architecture"""
 
-__version__ = "0.11.0"
+__version__ = "0.1.0"
