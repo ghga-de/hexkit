21911950bb81ffd2769c67ee934e85756defa55a
KerstenBreuer <kersten-breuer@outlook.com>
2023-01-31 09:01:08 +0000

    use new get object size method

--- hexkit/providers/s3/provider.py
+++ hexkit/providers/s3/provider.py
@@ -825,17 +825,9 @@ class S3ObjectStorage(
         another bucket (`dest_bucket_id` and `dest_object_id`).
         """
 
-        await self._assert_object_exists(
+        file_size = await self._get_object_size(
             bucket_id=source_bucket_id, object_id=source_object_id
         )
-        await self._assert_object_not_exists(
-            bucket_id=dest_bucket_id, object_id=dest_object_id
-        )
-
-        source_metadata = await self._get_object_metadata(
-            bucket_id=source_bucket_id, object_id=source_object_id
-        )
-        file_size = source_metadata["ContentLength"]
         part_size = calc_part_size(file_size=file_size)
 
         transfer_config = TransferConfig(
