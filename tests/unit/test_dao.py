8a4c959a424537f0e49958f2e10ca1838bbd5701
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-09-29 12:29:16 +0000

    removed unintended duplicate

--- tests/unit/test_dao.py
+++ tests/unit/test_dao.py
@@ -73,11 +73,6 @@ class FakeDaoFactory(DaoFactoryProtocol):
         """*To be implemented by the provider. Input validation is done outside of this
         method.*
         """
-        ...
-
-        """*To be implemented by the provider. Input validation is done outside of this
-        method.*"""
-
         raise NotImplementedError()
 
 
