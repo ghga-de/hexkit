aa870457fe4cf8331560aa59eee8d2132d515842
KerstenBreuer <kersten-breuer@outlook.com>
2024-02-04 09:57:43 +0000

    prepare mongodb provider for use in outbox

--- src/hexkit/providers/mongodb/provider.py
+++ src/hexkit/providers/mongodb/provider.py
@@ -25,9 +25,9 @@ import json
 from abc import ABC
 from collections.abc import AsyncGenerator, AsyncIterator, Collection, Mapping
 from contextlib import AbstractAsyncContextManager
-from typing import Any, Generic, Optional, Union, overload
+from typing import Any, Callable, Generic, Optional, Union, overload
 
-from motor.core import AgnosticClientSession, AgnosticCollection
+from motor.core import AgnosticCollection
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings
@@ -49,6 +49,26 @@ from hexkit.utils import FieldNotInModelError, validate_fields_in_model
 __all__ = ["MongoDbConfig", "MongoDbDaoFactory"]
 
 
+def document_to_dto(
+    document: dict[str, Any], *, id_field: str, dto_model: type[Dto]
+) -> Dto:
+    """Converts a document obtained from the MongoDB database into a DTO model-
+    compliant representation.
+    """
+    document[id_field] = document.pop("_id")
+    return dto_model.model_validate(document)
+
+
+def dto_to_document(dto: Dto, *, id_field: str) -> dict[str, Any]:
+    """Converts a DTO into a representation that is a compatible document for a
+    MongoDB Database.
+    """
+    document = json.loads(dto.model_dump_json())
+    document["_id"] = document.pop(id_field)
+
+    return document
+
+
 class MongoDbDaoBase(ABC, Generic[Dto]):
     """A base class with methods common to all MongoDB-based DAOs.
     This shall be used as base class for other MongoDB-based DAO implementations.
@@ -60,7 +80,8 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
         dto_model: type[Dto],
         id_field: str,
         collection: AgnosticCollection,
-        session: Optional[AgnosticClientSession] = None,
+        document_to_dto: Callable[[dict[str, Any]], Dto],
+        dto_to_document: Callable[[Dto], dict[str, Any]],
     ):
         """Initialize the DAO.
 
@@ -72,32 +93,18 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
                 (DAO implementation might use this field as primary key.)
             collection:
                 A collection object from the motor library.
-            session:
-                If transactional support is needed, please provide an
-                AsyncIOMotorClientSession that is within an active transaction. If None
-                is provided, every database operation is immediately commited. Defaults
-                to None.
+            document_to_dto:
+                A callable that takes a document obtained from the MongoDB database
+                and returns a DTO model-compliant representation.
+            dto_to_document:
+                A callable that takes a DTO model and returns a representation that is
+                a compatible document for a MongoDB database.
         """
         self._collection = collection
-        self._session = session
         self._dto_model = dto_model
         self._id_field = id_field
-
-    def _document_to_dto(self, document: dict[str, Any]) -> Dto:
-        """Converts a document obtained from the MongoDB database into a DTO model-
-        compliant representation.
-        """
-        document[self._id_field] = document.pop("_id")
-        return self._dto_model(**document)
-
-    def _dto_to_document(self, dto: Dto) -> dict[str, Any]:
-        """Converts a DTO into a representation that is a compatible document for a
-        MongoDB Database.
-        """
-        document = json.loads(dto.model_dump_json())
-        document["_id"] = document.pop(self._id_field)
-
-        return document
+        self._document_to_dto = document_to_dto
+        self._dto_to_document = dto_to_document
 
     async def get_by_id(self, id_: str) -> Dto:
         """Get a resource by providing its ID.
@@ -111,10 +118,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
         Raises:
             ResourceNotFoundError: when resource with the specified id_ was not found
         """
-        document = await self._collection.find_one(
-            {"_id": id_},
-            session=self._session,
-        )
+        document = await self._collection.find_one({"_id": id_})
 
         if document is None:
             raise ResourceNotFoundError(id_=id_)
@@ -134,9 +138,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
                 when resource with the id specified in the dto was not found
         """
         document = self._dto_to_document(dto)
-        result = await self._collection.replace_one(
-            {"_id": document["_id"]}, document, session=self._session
-        )
+        result = await self._collection.replace_one({"_id": document["_id"]}, document)
 
         if result.matched_count == 0:
             raise ResourceNotFoundError(id_=document["_id"])
@@ -153,7 +155,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
         Raises:
             ResourceNotFoundError: when resource with the specified id_ was not found
         """
-        result = await self._collection.delete_one({"_id": id_}, session=self._session)
+        result = await self._collection.delete_one({"_id": id_})
 
         if result.deleted_count == 0:
             raise ResourceNotFoundError(id_=id_)
@@ -197,7 +199,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
         hits = self.find_all(mapping=mapping)
 
         try:
-            document = await hits.__anext__()
+            dto = await hits.__anext__()
         except StopAsyncIteration as error:
             raise NoHitsFoundError(mapping=mapping) from error
 
@@ -205,7 +207,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
             _ = await hits.__anext__()
         except StopAsyncIteration:
             # This is expected:
-            return document
+            return dto
 
         raise MultipleHitsFoundError(mapping=mapping)
 
@@ -227,7 +229,7 @@ class MongoDbDaoBase(ABC, Generic[Dto]):
             mapping = dict(mapping)
             mapping["_id"] = mapping.pop(self._id_field)
 
-        cursor = self._collection.find(filter=mapping, session=self._session)
+        cursor = self._collection.find(filter=mapping)
 
         async for document in cursor:
             yield self._document_to_dto(document)
@@ -265,7 +267,8 @@ class MongoDbDaoSurrogateId(MongoDbDaoBase[Dto], Generic[Dto, DtoCreation_contra
         id_field: str,
         collection: AgnosticCollection,
         id_generator: AsyncGenerator[str, None],
-        session: Optional[AgnosticClientSession] = None,
+        document_to_dto: Callable[[dict[str, Any]], Dto],
+        dto_to_document: Callable[[Dto], dict[str, Any]],
     ):
         """Initialize the DAO.
 
@@ -282,17 +285,21 @@ class MongoDbDaoSurrogateId(MongoDbDaoBase[Dto], Generic[Dto, DtoCreation_contra
                 (DAO implementation might use this field as primary key.)
             collection:
                 A collection object from the motor library.
-            session:
-                If transactional support is needed, please provide an
-                AsyncIOMotorClientSession that is within an active transaction. If None
-                is provided, every database operation is immediately commited. Defaults
-                to None.
+            id_generator:
+                A generator to continuously generate new IDs for new resources.
+            document_to_dto:
+                A callable that takes a document obtained from the MongoDB database
+                and returns a DTO model-compliant representation.
+            dto_to_document:
+                A callable that takes a DTO model and returns a representation that is
+                a compatible document for a MongoDB database.
         """
         super().__init__(
             dto_model=dto_model,
             id_field=id_field,
             collection=collection,
-            session=session,
+            document_to_dto=document_to_dto,
+            dto_to_document=dto_to_document,
         )
 
         self._dto_creation_model = dto_creation_model
@@ -318,7 +325,7 @@ class MongoDbDaoSurrogateId(MongoDbDaoBase[Dto], Generic[Dto, DtoCreation_contra
         full_dto = self._dto_model(**data)
 
         document = self._dto_to_document(full_dto)
-        await self._collection.insert_one(document, session=self._session)
+        await self._collection.insert_one(document)
 
         return full_dto
 
@@ -352,7 +359,7 @@ class MongoDbDaoNaturalId(MongoDbDaoBase[Dto]):
                 when a resource with the ID specified in the dto does already exist.
         """
         document = self._dto_to_document(dto)
-        await self._collection.insert_one(document, session=self._session)
+        await self._collection.insert_one(document)
 
     async def upsert(self, dto: Dto) -> None:
         """Update the provided resource if it already exists, create it otherwise.
@@ -364,7 +371,7 @@ class MongoDbDaoNaturalId(MongoDbDaoBase[Dto]):
         """
         document = self._dto_to_document(dto)
         await self._collection.replace_one(
-            {"_id": document["_id"]}, document, session=self._session, upsert=True
+            {"_id": document["_id"]}, document, upsert=True
         )
 
 
@@ -467,11 +474,18 @@ class MongoDbDaoFactory(DaoFactoryProtocol):
 
         collection = self._db[name]
 
+        document_to_dto_ = lambda document: document_to_dto(
+            document=document, id_field=id_field, dto_model=dto_model
+        )
+        dto_to_document_ = lambda dto: dto_to_document(dto=dto, id_field=id_field)
+
         if dto_creation_model is None:
             return MongoDbDaoNaturalId(
                 collection=collection,
                 dto_model=dto_model,
                 id_field=id_field,
+                document_to_dto=document_to_dto_,
+                dto_to_document=dto_to_document_,
             )
 
         return MongoDbDaoSurrogateId(
@@ -480,4 +494,6 @@ class MongoDbDaoFactory(DaoFactoryProtocol):
             dto_creation_model=dto_creation_model,
             id_field=id_field,
             id_generator=id_generator,
+            document_to_dto=document_to_dto_,
+            dto_to_document=dto_to_document_,
         )
