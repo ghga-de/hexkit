152e7575b9b8d7dcbd3724a1e42bae48f11873e7
TheByronHimes <TheByronHimes@gmail.com>
2024-07-29 14:49:51 +0000

    Add caplog record checking util functions

--- tests/fixtures/utils.py
+++ tests/fixtures/utils.py
@@ -18,6 +18,7 @@
 import logging
 import os
 from pathlib import Path
+from typing import Literal
 
 import pytest
 import yaml
@@ -49,3 +50,58 @@ def root_logger_reset():
     # reset level and handlers
     root.setLevel(original_level)
     root.handlers = root_handlers
+
+
+LogLevel = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "FATAL"]
+
+
+def assert_logged(
+    level: LogLevel,
+    message: str,
+    records: list[logging.LogRecord],
+    parse: bool = True,
+) -> str:
+    """Assert that a log message was logged with the given level and message.
+
+    Args:
+    - `level`: The log level to check for.
+    - `message`: The message to check for.
+    - `records`: The log records to check (usually `caplog.records`)
+    - `parse`: Whether to parse the message with the arguments from the log record.
+
+    If a match is found, the parsed message is return (regardless of the value of `parse`).
+    """
+    for i, record in enumerate(records):
+        msg_to_inspect = str(record.msg) % record.args if parse else record.msg
+        if record.levelname == level and msg_to_inspect == message:
+            del records[i]
+            return msg_to_inspect if parse else str(record.msg) % record.args
+    else:
+        assert False, f"Log message not found: {level} - {message}"
+
+
+def assert_not_logged(
+    level: LogLevel,
+    message: str,
+    records: list[logging.LogRecord],
+    parse: bool = True,
+):
+    """Assert that a log message was not logged with the given level and message.
+
+    Args:
+    - `level`: The log level to check for.
+    - `message`: The message to check for.
+    - `records`: The log records to check (usually `caplog.records`)
+    - `parse`: Whether to parse the message with the arguments from the log record.
+    """
+    for record in records:
+        msg_to_inspect = str(record.msg) % record.args if parse else record.msg
+        if record.levelname == level and msg_to_inspect == message:
+            assert False, f"Log message found: {level} - {message}"
+
+
+@pytest.fixture(name="caplog_debug")
+def caplog_debug_fixture(caplog):
+    """Convenience fixture to set the log level of caplog to debug for a test."""
+    caplog.set_level(logging.DEBUG)
+    yield caplog
