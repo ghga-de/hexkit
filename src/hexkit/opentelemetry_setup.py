30ea79c762d74eeec28fbb3f1235988c9b63ffc6
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-05-05 17:03:09 +0000

    OTel setup code cleanup

--- src/hexkit/opentelemetry_setup.py
+++ src/hexkit/opentelemetry_setup.py
@@ -14,49 +14,91 @@
 # limitations under the License.
 """OpenTelemetry specific configuration code. This is gated behind the opentelemetry extra."""
 
+import logging
 import os
-from collections import defaultdict
-from functools import partial
 from typing import Callable, Literal, Optional
 
 from opentelemetry import trace
+from opentelemetry.environment_variables import (
+    OTEL_LOGS_EXPORTER,
+    OTEL_METRICS_EXPORTER,
+    OTEL_TRACES_EXPORTER,
+)
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.environment_variables import OTEL_EXPORTER_OTLP_PROTOCOL
+from opentelemetry.sdk.environment_variables import (
+    OTEL_EXPORTER_OTLP_PROTOCOL,
+    OTEL_SDK_DISABLED,
+)
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.sampling import ParentBasedTraceIdRatio
 
-tracer_decorator = Callable[[Optional[str], bool, bool], Callable]
+TRACER: Optional["SpanTracer"] = None
 
-CONFIGURED_TRACERS: dict[str, "SpanTracer"] = dict()
+logger = logging.getLogger(__name__)
 
 
 def configure_tracer(
+    *,
     service_name: str,
-    protocol: Literal["grpc", "http/protobuf"] = "http/protobuf",
     enable_otel: bool = False,
+    protocol: Literal["grpc", "http/protobuf"] = "http/protobuf",
+    sampling_rate: float = 1,
 ):
     """Set up a global tracer for a specific service using the given exporter protocol."""
+    global TRACER
     # opentelemetry distro sets this to grpc, but in the current context http/protobuf is preferred
     os.environ.setdefault(OTEL_EXPORTER_OTLP_PROTOCOL, protocol)
-    # In any case disable OpenTelemetry metrics and logs explicitly
+    # Disable OpenTelemetry metrics and logs explicitly as they are not processed in the backend currently
+    # This overwrites the defaults of `otlp` set in opentelemetry distro
+    os.environ.setdefault(OTEL_METRICS_EXPORTER, "none")
+    os.environ.setdefault(OTEL_LOGS_EXPORTER, "none")
 
     if enable_otel:
+        if TRACER is not None:
+            logger.warning(
+                "OpenTelemetry configuration code should only be run once. "
+                "If it has been run with a different service name than %s before, "
+                "the tracer and resource name will likely be wrong in some cases."
+            )
         resource = Resource(attributes={SERVICE_NAME: service_name})
-        trace_provider = TracerProvider(resource=resource)
+        # Replace the default static sampler with a probabilistic one
+        # With the default sampling rate, behaviour does not change, but this allows to
+        # introduce head sampling by adjusting a config option on the service side later on
+        if sampling_rate > 1:
+            sampling_rate = 1
+            logger.info(
+                "Provided sampling rate has to be between 0 and 1. Adjusted from %i to 1.",
+                sampling_rate,
+            )
+        elif sampling_rate < 0:
+            sampling_rate = 0
+            logger.info(
+                "Provided sampling rate has to be between 0 and 1. Adjusted from %i to 0.",
+                sampling_rate,
+            )
+        # Initiate a sampler that honors parent span sampling decisions
+        # This should consistently yield full traces within a service but not necessarily
+        # across service boundaries
+        sampler = ParentBasedTraceIdRatio(rate=sampling_rate)
+
+        # Initialize service specific TracerProvider
+        trace_provider = TracerProvider(resource=resource, sampler=sampler)
         processor = BatchSpanProcessor(OTLPSpanExporter())
         trace_provider.add_span_processor(processor)
         trace.set_tracer_provider(trace_provider)
-        global CONFIGURED_TRACERS, CONFIGURED_DECORATORS
-        CONFIGURED_TRACERS[service_name] = SpanTracer(service_name)
-        partial(_with_configured_tracer, service_name=service_name)
+        TRACER = SpanTracer(service_name)
     else:
-        ...
+        # Currently OTEL_SDK_DISABLED doesn't seem to be honored by all implementations yet
+        # It seems to be working well enough for the Python implementation, but to be on
+        # the safe side, let's explicitly disable the trace exporter for now
+        os.environ.setdefault(OTEL_TRACES_EXPORTER, "none")
+        os.environ.setdefault(OTEL_SDK_DISABLED, "true")
 
 
-def _with_configured_tracer(
+def with_configured_tracer(
     *,
-    service_name: str | None = None,
     record_exception: bool = False,
     set_status_on_exception: bool = False,
 ) -> Callable:
@@ -66,25 +108,16 @@ def _with_configured_tracer(
     """
 
     def wrapper(function: Callable):
-        # Caller did not have any time to do initialization yet or otel is disabled
-        if service_name is None:
+        # Caller did not have any time to populate from config yet or otel is disabled
+        if TRACER is None:
             return function
-        tracer = CONFIGURED_TRACERS.get(service_name)
-        if tracer is None:
-            return function
-
-        # There's a configured tracer present
-        return tracer.start_span(
+        # Return decorated function
+        return TRACER.start_span(
             record_exception=record_exception,
             set_status_on_exception=set_status_on_exception,
         )(function)
 
     return wrapper
-
-
-CONFIGURED_DECORATORS: dict[str, tracer_decorator] = defaultdict(
-    _with_configured_tracer
-)
 
 
 class SpanTracer:
