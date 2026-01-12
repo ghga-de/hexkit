7656ee769cbaf8a77f1b88ccd26e20243bf145ed
Byron Himes <thebyronhimes@gmail.com>
2026-01-12 11:18:08 +0100

    Support limited nosql for InMemDao (GSI-2080) (#205)
    * Enable limited mongo query language in InMem dao
    
    * Bump version from 7.2.1 -> 7.3.0
    
    * Add undocumented improvements to MQL predicate interpretation
    
    * Fix problems with predicate parser
    
    * Disallow nesting logical operators under $not and add tests
    
    * Add comparison tests for real mongodb and inmemdao
    
    Also remove neq operator because it's not real
    
    * Document predicate classes and functions
    
    * Simplify comparisons
    
    * Fix capitalization woes
    
    * Make mypy be quiet
    
    * Use dynamic class name in repr and switch to Protocol
    
    * Fix dot notation so it errors on empty list
    
    * Implicitly define unsupported mql operators
    
    * Fold build_xyz_predicate functions into __init__() and fetch fn early
    
    * Parametrize test
    
    * Make field validation work with dot notation
    
    * Add missing async to unit tests
    
    * Store resources in InMemDao mostly closely to how they are stored in a real DB
    
    Fix nesting
    
    Make sure $exists works with nested fields
    
    * Add further tests for comparison between mock dao and actual dao

---
+++ tests/integration/mongodb/test_in_mem_dao_against_actual_mongo.py
@@ -0,0 +1,343 @@
+# Copyright 2021 - 2025 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests meant to verify the reliability of the InMemDao"""
+
+from collections.abc import AsyncGenerator, Mapping
+from datetime import datetime, timedelta
+from typing import Any
+
+import pytest
+import pytest_asyncio
+from pydantic import BaseModel, Field
+
+from hexkit.protocols.dao import Dao
+from hexkit.providers.mongodb.provider import MongoDbDaoFactory
+from hexkit.providers.mongodb.testutils import (
+    MongoDbFixture,
+    mongodb_container_fixture,  # noqa: F401
+    mongodb_fixture,  # noqa: F401
+)
+from hexkit.providers.testing import new_mock_dao_class
+from hexkit.providers.testing.dao import BaseInMemDao
+from hexkit.utils import now_utc_ms_prec
+
+pytestmark = pytest.mark.asyncio()
+
+DATE1 = now_utc_ms_prec() + timedelta(weeks=1)
+DATE2 = now_utc_ms_prec() + timedelta(weeks=2)
+DATE3 = now_utc_ms_prec() + timedelta(weeks=3)
+DATE4 = now_utc_ms_prec() + timedelta(weeks=4)
+
+
+class OtherData(BaseModel):
+    """A model with the amount of the object sold last week and next restock date"""
+
+    sold_last_week: int
+    next_restock: datetime
+
+
+class InventoryItem(BaseModel):
+    """A model that can be used for testing"""
+
+    title: str
+    count: int
+    other_data: OtherData
+
+
+class InventoryCategory(BaseModel):
+    """A model that features nesting and lists"""
+
+    title: str
+    items: list[InventoryItem] = Field(default=[])
+    top_item: InventoryItem = Field(default=...)
+
+
+# Real DAOs
+InventoryItemDao = Dao[InventoryItem]
+InventoryCategoryDao = Dao[InventoryCategory]
+
+# Mock DAOs
+MockItemDaoClass = new_mock_dao_class(dto_model=InventoryItem, id_field="title")
+MockCategoryDaoClass = new_mock_dao_class(dto_model=InventoryCategory, id_field="title")
+
+# Define some test objects
+APPLES = InventoryItem(
+    title="apples",
+    count=101,
+    other_data=OtherData(sold_last_week=25, next_restock=DATE1),
+)
+BROCCOLI = InventoryItem(
+    title="broccoli",
+    count=7,
+    other_data=OtherData(sold_last_week=18, next_restock=DATE1),
+)
+CELERY = InventoryItem(
+    title="celery",
+    count=40,
+    other_data=OtherData(sold_last_week=12, next_restock=DATE2),
+)
+PRODUCE = InventoryCategory(
+    title="produce", items=[APPLES, BROCCOLI, CELERY], top_item=APPLES
+)
+
+BRAKE_PADS = InventoryItem(
+    title="brake pads",
+    count=254,
+    other_data=OtherData(sold_last_week=35, next_restock=DATE3),
+)
+CHAIN = InventoryItem(
+    title="chain",
+    count=500,
+    other_data=OtherData(sold_last_week=55, next_restock=DATE4),
+)
+BIKE_PARTS = InventoryCategory(
+    title="bike parts", items=[BRAKE_PADS, CHAIN], top_item=BRAKE_PADS
+)
+CATEGORIES_COLL_NAME = "categories"
+ITEMS_COLL_NAME = "items"
+
+
+@pytest_asyncio.fixture(name="mock_category_dao")
+async def populated_category_dao() -> BaseInMemDao[InventoryCategory]:
+    """Provides a populated InMemDao for InventoryCategory objects"""
+    dao = MockCategoryDaoClass()
+    await dao.insert(PRODUCE)
+    await dao.insert(BIKE_PARTS)
+    return dao
+
+
+@pytest_asyncio.fixture(name="mock_item_dao")
+async def populated_item_dao() -> BaseInMemDao[InventoryItem]:
+    """Provides a populated InMemDao for InventoryItem objects"""
+    dao = MockItemDaoClass()
+    await dao.insert(APPLES)
+    await dao.insert(BROCCOLI)
+    await dao.insert(CELERY)
+    await dao.insert(BRAKE_PADS)
+    await dao.insert(CHAIN)
+    return dao
+
+
+@pytest_asyncio.fixture(name="real_category_dao")
+async def populated_real_category_dao(
+    mongodb: MongoDbFixture,
+) -> AsyncGenerator[Dao[InventoryCategory]]:
+    """Provides a ready-to-use synchronous db with test data inserted"""
+    async with MongoDbDaoFactory.construct(config=mongodb.config) as dao_factory:
+        dao = await dao_factory.get_dao(
+            name=CATEGORIES_COLL_NAME,
+            dto_model=InventoryCategory,
+            id_field="title",
+        )
+        for x in [PRODUCE, BIKE_PARTS]:
+            await dao.insert(x)
+        yield dao
+
+
+@pytest_asyncio.fixture(name="real_item_dao")
+async def populated_real_item_dao(
+    mongodb: MongoDbFixture,
+) -> AsyncGenerator[Dao[InventoryItem]]:
+    """Provides a ready-to-use InventoryItem DAO with test data inserted"""
+    async with MongoDbDaoFactory.construct(config=mongodb.config) as dao_factory:
+        dao = await dao_factory.get_dao(
+            name=ITEMS_COLL_NAME,
+            dto_model=InventoryItem,
+            id_field="title",
+        )
+        for x in [APPLES, BROCCOLI, CELERY, BRAKE_PADS, CHAIN]:
+            await dao.insert(x)
+        yield dao
+
+
+@pytest.mark.parametrize(
+    "mapping, results",
+    [
+        pytest.param(
+            {"top_item.other_data.sold_last_week": {"$exists": True}},
+            ["bike parts", "produce"],
+            id="ExistsOnNestedField",
+        ),
+        pytest.param(
+            {"top_item": {"$exists": True}},
+            ["bike parts", "produce"],
+            id="ExistsTopLevelField",
+        ),
+        pytest.param(
+            {"top_item.count": {"$gte": 100}},
+            ["bike parts", "produce"],
+            id="GteOnNestedField",
+        ),
+        pytest.param(
+            {"top_item.count": {"$lte": 101, "$gte": 100}},
+            ["produce"],
+            id="RangeQueryWithGteAndLte",
+        ),
+        pytest.param(
+            {"top_item.other_data.sold_last_week": {"$nin": [25, 30, 40]}},
+            ["bike parts"],
+            id="NinOperator",
+        ),
+        pytest.param(
+            {
+                "$and": [
+                    {"top_item.count": {"$gt": 50}},
+                    {"top_item.count": {"$lt": 200}},
+                ]
+            },
+            ["produce"],
+            id="AndOperator",
+        ),
+        pytest.param(
+            {
+                "$nor": [
+                    {"top_item.count": {"$lt": 100}},
+                    {"top_item.count": {"$gt": 300}},
+                ]
+            },
+            ["bike parts", "produce"],
+            id="NorOperator",
+        ),
+        pytest.param(
+            {"top_item.count": {"$not": {"$lt": 150}}},
+            ["bike parts"],
+            id="NotOperator",
+        ),
+    ],
+)
+async def test_with_category_dao(
+    real_category_dao, mock_category_dao, mapping: Mapping[str, Any], results: list[str]
+):
+    """Test that dot notation is interpreted by the InMemDao"""
+    mongodb_results = [
+        x.title async for x in real_category_dao.find_all(mapping=mapping)
+    ]
+    inmem_results = [x.title async for x in mock_category_dao.find_all(mapping=mapping)]
+    assert sorted(mongodb_results) == results, "MongoDB results not as expected"
+    assert sorted(inmem_results) == results, "Mock DAO results not as expected"
+
+
+@pytest.mark.parametrize(
+    "mapping, results",
+    [
+        pytest.param(
+            {"count": {"$lt": 200, "$ne": 7}},
+            ["apples", "celery"],
+            id="OneFieldMultipleOps",
+        ),
+        pytest.param(
+            {
+                "$or": [{"count": {"$gt": 200}}, {"count": {"$eq": 7}}],
+                "title": {"$in": ["chain", "broccoli", "apples"]},
+            },
+            ["broccoli", "chain"],
+            id="MultipleFieldsMultipleOps",
+        ),
+        pytest.param(
+            {"other_data.next_restock": {"$gte": DATE1, "$lte": DATE2}},
+            ["apples", "broccoli", "celery"],
+            id="RangeQueryOnDateField",
+        ),
+        pytest.param(
+            {
+                "$and": [
+                    {"count": {"$gte": 40}},
+                    {"other_data.sold_last_week": {"$lt": 50}},
+                ]
+            },
+            ["apples", "brake pads", "celery"],
+            id="AndOperatorComplex",
+        ),
+        pytest.param(
+            {
+                "$nor": [
+                    {"count": {"$lt": 50}},
+                    {"count": {"$gt": 400}},
+                ]
+            },
+            ["apples", "brake pads"],
+            id="NorOperatorComplex",
+        ),
+        pytest.param(
+            {"count": {"$not": {"$gte": 100}}},
+            ["broccoli", "celery"],
+            id="NotOperatorSimple",
+        ),
+        pytest.param(
+            {
+                "$or": [
+                    {"$and": [{"count": {"$gte": 100}}, {"count": {"$lte": 300}}]},
+                    {"other_data.sold_last_week": {"$eq": 55}},
+                ]
+            },
+            ["apples", "brake pads", "chain"],
+            id="NestedLogicalOperators",
+        ),
+        pytest.param(
+            {
+                "other_data.next_restock": {"$eq": DATE1},
+                "count": {"$ne": 7},
+            },
+            ["apples"],
+            id="DateEqualityWithNe",
+        ),
+    ],
+)
+async def test_with_item_dao(
+    real_item_dao, mock_item_dao, mapping: Mapping[str, Any], results: list[str]
+):
+    """Test that dot notation is interpreted by the InMemDao"""
+    mongodb_results = [x.title async for x in real_item_dao.find_all(mapping=mapping)]
+    inmem_results = [x.title async for x in mock_item_dao.find_all(mapping=mapping)]
+    assert sorted(mongodb_results) == results, "MongoDB results not as expected"
+    assert sorted(inmem_results) == results, "Mock DAO results not as expected"
+
+
+@pytest.mark.parametrize(
+    "mapping, results",
+    [
+        pytest.param(
+            {"top_item.other_data": APPLES.other_data.model_dump()},
+            ["produce"],
+            id="SimpleNest",
+        ),
+        pytest.param(
+            {"title.notreal": None},
+            ["bike parts", "produce"],
+            id="MatchNonExistentField",
+        ),
+    ],
+)
+async def test_also_on_non_mql(
+    real_category_dao, mock_category_dao, mapping: Mapping[str, Any], results: list[str]
+):
+    """Tests for direct equality of a nested field with MQL enabled and disabled"""
+    mongodb_results = [
+        x.title async for x in real_category_dao.find_all(mapping=mapping)
+    ]
+    inmem_results = [x.title async for x in mock_category_dao.find_all(mapping=mapping)]
+    assert sorted(mongodb_results) == results, "MongoDB results not as expected"
+    assert sorted(inmem_results) == results, "Mock DAO results not as expected"
+
+    # Make sure that DAOs that don't handle MQL still support dot notation
+    CategoryDaoClassNoMQL = new_mock_dao_class(  # noqa: N806
+        dto_model=InventoryCategory, id_field="title", handle_mql=False
+    )
+    dao = CategoryDaoClassNoMQL()
+    await dao.insert(PRODUCE)
+    await dao.insert(BIKE_PARTS)
+    nomql_results = [x.title async for x in dao.find_all(mapping=mapping)]
+    assert nomql_results == inmem_results, "Non-MQL Mock DAO results not as expected"
--- tests/unit/test_in_mem_dao.py
+++ tests/unit/test_in_mem_dao.py
@@ -15,11 +15,21 @@
 
 """Testing the in-memory DAO."""
 
+from typing import Any
+
 import pytest
 from pydantic import BaseModel
 
 from hexkit.protocols.dao import NoHitsFoundError, ResourceNotFoundError
 from hexkit.providers.testing import MockDAOEmptyError, new_mock_dao_class
+from hexkit.providers.testing.dao import (
+    ComparisonPredicate,
+    DataTypePredicate,
+    LogicalPredicate,
+    MQLError,
+    Predicate,
+    build_predicates,
+)
 
 pytestmark = pytest.mark.asyncio()
 
@@ -127,29 +137,20 @@ async def test_find_all():
     await dao.insert(bat)
 
     # Get resources with count=1, sorted alphabetically by title
-    results = sorted(
-        [x async for x in dao.find_all(mapping={"count": 1})], key=lambda x: x.title
-    )
-    assert results
-    assert len(results) == 2
-    assert results[0].title == "Brick"
-    assert results[1].title == "Shovel"
+    results = sorted([x.title async for x in dao.find_all(mapping={"count": 1})])
+    assert results == ["Brick", "Shovel"]
 
     # Filter by the id field
-    results = [x async for x in dao.find_all(mapping={"title": "Bat"})]
-    assert results
-    assert results[0] is not bat and (bat.model_dump() == results[0].model_dump())
+    results = [x.title async for x in dao.find_all(mapping={"title": "Bat"})]
+    assert results == ["Bat"]
 
     # Look for something that doesn't exist
-    results = [x async for x in dao.find_all(mapping={"title": "Broom"})]
+    results = [x.title async for x in dao.find_all(mapping={"title": "Broom"})]
     assert not results
 
     # Get everything
-    results = sorted([x async for x in dao.find_all(mapping={})], key=lambda x: x.title)
-    assert len(results) == 3
-    assert [x.model_dump() for x in results] == [
-        x.model_dump() for x in [bat, brick, shovel]
-    ]
+    results = sorted([x.title async for x in dao.find_all(mapping={})])
+    assert results == ["Bat", "Brick", "Shovel"]
 
 
 async def test_get_by_id():
@@ -168,3 +169,318 @@ async def test_get_by_id():
     this_pumpkin = await dao.get_by_id("Pumpkin")
     assert this_pumpkin is not that_pumpkin
     assert this_pumpkin.model_dump() == that_pumpkin.model_dump()
+
+
+@pytest.mark.parametrize(
+    "mapping,expected",
+    [
+        ({"title": {"$eq": "Brick"}}, ["Brick"]),
+        ({"count": {"$gt": 1}}, ["Shovel", "Tophat"]),
+        ({"count": {"$gte": 1}}, ["Brick", "Shovel", "Tophat"]),
+        ({"title": {"$in": ["Brick", "Shovel"]}}, ["Brick", "Shovel"]),
+        ({"count": {"$lt": 3}}, ["Brick", "Shovel"]),
+        ({"count": {"$lte": 3}}, ["Brick", "Shovel", "Tophat"]),
+        ({"title": {"$ne": "Tophat"}}, ["Brick", "Shovel"]),
+        ({"title": {"$nin": ["This", "is", "my", "Tophat"]}}, ["Brick", "Shovel"]),
+    ],
+)
+async def test_mql_comparison_ops(mapping: dict[str, Any], expected: list[str]):
+    """Test the different MQL comparison operators"""
+    dao = DaoClass()
+    brick = InventoryItem(title="Brick", count=1)
+    shovel = InventoryItem(title="Shovel", count=2)
+    tophat = InventoryItem(title="Tophat", count=3)
+    await dao.insert(brick)
+    await dao.insert(shovel)
+    await dao.insert(tophat)
+
+    results = sorted([x.title async for x in dao.find_all(mapping=mapping)])
+    assert results == expected
+
+
+async def test_build_predicates_empty_mapping():
+    """Verify that running build_predicates() on an empty dict returns an empty list"""
+    assert build_predicates(dict()) == []
+
+
+async def test_basic_nesting():
+    """Verify nesting interpretation in predicate building"""
+    mapping = {
+        "field1": {"$eq": {"fieldX": 123}},
+        "field1.fieldY": {"$gt": 9001},
+    }
+    expected_predicates = [
+        ComparisonPredicate(op="$eq", field="field1", target_value={"fieldX": 123}),
+        ComparisonPredicate(op="$gt", field="field1.fieldY", target_value=9001),
+    ]
+    assert build_predicates(mapping) == expected_predicates
+
+    # No error here because MongoDB interprets {"$gt": 9001} literally
+    mapping = {
+        "field1": {
+            "$eq": {
+                "fieldX": 123,
+                "fieldY": {"$gt": 9001},
+            }
+        }
+    }
+    expected_predicates = [
+        ComparisonPredicate(
+            op="$eq",
+            field="field1",
+            target_value={"fieldX": 123, "fieldY": {"$gt": 9001}},
+        ),
+    ]
+
+
+@pytest.mark.parametrize("op", ["$eq", "$ne"])
+async def test_eq_ne(op: str):
+    """Test the $eq and $ne operators in predicate building"""
+    # Standard, proper usage
+    for value in [17, False, ["foo", "bar"], {}, {"$gt": "y"}, b"baz", None]:
+        mapping = {"my_field": {op: value}}
+        expected_predicates = [
+            ComparisonPredicate(op=op, field="my_field", target_value=value)
+        ]
+        assert build_predicates(mapping) == expected_predicates
+
+    # Inverted nesting
+    with pytest.raises(MQLError):
+        _ = build_predicates({op: {"age": [32, 66, 83]}})
+
+
+@pytest.mark.parametrize("op", ["$gt", "$lt", "$gte", "$lte"])
+async def test_gt_lt_gte_lte(op: str):
+    """Test the $gt, $lt, $gte, and $lte operators in predicate building"""
+    mapping = {"count": {op: 0}}
+    expected_predicates = [ComparisonPredicate(op=op, field="count", target_value=0)]
+    assert build_predicates(mapping) == expected_predicates
+
+
+@pytest.mark.parametrize("op", ["$in", "$nin"])
+async def test_in_nin(op: str):
+    """Test the $in and $nin operators in predicate building"""
+    # Standard, proper usage, including empty values
+    arrays = [[1, 2, 3], (1, 2, 3), [], ()]
+    for arr in arrays:
+        mapping = {"count": {op: arr}}
+        assert build_predicates(mapping) == [
+            ComparisonPredicate(op=op, field="count", target_value=arr)
+        ]
+
+    # Non-list values
+    for nonlist_value in ["abc", {1, 2, 3}, {"some": "dict"}]:
+        with pytest.raises(MQLError):
+            _ = build_predicates({"f1": {op: nonlist_value}})
+
+    # Inverted nesting
+    with pytest.raises(MQLError):
+        _ = build_predicates({op: {"age": [32, 66, 83]}})
+
+
+async def test_exists():
+    """Test the $exists operator in predicate building"""
+    for value in [True, False]:
+        mapping = {"new_field": {"$exists": value}}
+        assert build_predicates(mapping) == [
+            DataTypePredicate(op="$exists", field="new_field", target_value=value)
+        ]
+
+    # Inverted nesting
+    with pytest.raises(MQLError):
+        _ = build_predicates({"$exists": {"new_field": value}})
+
+    # Non-boolean values
+    for bad_value in [0, 1, "True", {"set", "of", "strs"}, [9, 8, 7], b"bytestring"]:
+        with pytest.raises(MQLError):
+            _ = build_predicates({"new_field": {"$exists": bad_value}})
+
+
+@pytest.mark.parametrize("op", ["$and", "$nor", "$or"])
+async def test_and(op: str):
+    """Test the $and operator"""
+    # Standard, proper usage
+    mapping: dict[str, Any] = {
+        op: [
+            {"field1": {"$gt": 9001}},
+            {"field2": {"$eq": ["some", "list", "values"]}},
+        ]
+    }
+    expected_predicates: list[Predicate] = [
+        LogicalPredicate(op=op, field=None, mapping=mapping[op])
+    ]
+    results = build_predicates(mapping)
+    assert results == expected_predicates
+    assert results[0]._conditions == [  # type: ignore
+        ComparisonPredicate(op="$gt", field="field1", target_value=9001),
+        ComparisonPredicate(
+            op="$eq", field="field2", target_value=["some", "list", "values"]
+        ),
+    ]
+
+    # Empty list
+    with pytest.raises(MQLError):
+        _ = build_predicates({op: []})
+
+    # Does not use a list
+    with pytest.raises(MQLError):
+        _ = build_predicates({op: True})
+
+    # Inverted nesting
+    with pytest.raises(MQLError):
+        _ = build_predicates({"field1": {op: [{"$lt": 99}]}})
+
+    # Only one condition
+    mapping = {op: [{"field": {"$eq": 1}}]}
+    expected_predicates = [LogicalPredicate(op=op, field=None, mapping=mapping[op])]
+    results = build_predicates(mapping)
+    assert results == expected_predicates
+    assert results[0]._conditions == [  # type: ignore
+        ComparisonPredicate(op="$eq", field="field", target_value=1)
+    ]
+
+    # Demonstration of implicit $and (produces list of ComparisonPredicate)
+    mapping = {"count": {"$gt": 1, "$lt": 10}}
+    expected_predicates = [
+        ComparisonPredicate(op="$gt", field="count", target_value=1),
+        ComparisonPredicate(op="$lt", field="count", target_value=10),
+    ]
+    build_predicates(mapping)
+
+
+async def test_not():
+    """Test the $not operator"""
+    mapping: dict[str, Any] = {"field1": {"$not": {"$eq": 0}}}
+    expected_predicates = [
+        LogicalPredicate(op="$not", field="field1", mapping={"$eq": 0})
+    ]
+    results = build_predicates(mapping)
+    assert results == expected_predicates
+    assert results[0]._conditions == [  # type: ignore
+        ComparisonPredicate(op="$eq", field="field1", target_value=0)
+    ]
+    # Does not contain an expression
+    with pytest.raises(MQLError):
+        _ = build_predicates({"field2": {"$not": {}}})
+
+    # Does not point to an operator expression
+    with pytest.raises(MQLError):
+        _ = build_predicates({"field3": {"$not": {"k": "v"}}})
+
+    # Not nested within a field
+    with pytest.raises(MQLError):
+        _ = build_predicates({"$not": {"field4": {"$eq": 0}}})
+
+    # Does not point to a dict
+    with pytest.raises(MQLError):
+        _ = build_predicates({"field5": {"$not": ["$eq", 123]}})
+
+    # Nesting is inverted
+    with pytest.raises(MQLError):
+        _ = build_predicates({"$not": {"field6": {"$eq": True}}})
+
+    # Points to other logical operator aside from another $not
+    for logop in ["$and", "$nor", "$or"]:
+        mapping = {"count": {"$not": {logop: [{"$exists": True}, {"$gt": "5"}]}}}
+        with pytest.raises(MQLError):
+            _ = build_predicates(mapping)
+
+    # Nested $nots
+    mapping = {"verdict": {"$not": {"$not": {"$not": {"$ne": "guilty"}}}}}
+    expected_predicates = [
+        LogicalPredicate(
+            op="$not", field="verdict", mapping={"$not": {"$not": {"$ne": "guilty"}}}
+        )
+    ]
+
+    results = build_predicates(mapping)
+    assert results == expected_predicates
+    assert isinstance(results[0]._conditions[0], LogicalPredicate)  # type: ignore
+    assert isinstance(results[0]._conditions[0]._conditions[0], LogicalPredicate)  # type: ignore
+    assert results[0]._conditions[0]._conditions[0]._conditions == [  # type: ignore
+        ComparisonPredicate(op="$ne", field="verdict", target_value="guilty")
+    ]
+
+
+async def test_build_predicates_complex():
+    """Test query predicate parsing for a mapping that uses nested logical operators"""
+    mapping: dict[str, Any] = {
+        "$and": [
+            {"count": {"$exists": True}},
+            {
+                "$or": [
+                    {"count": {"$in": [11, 17, 81]}},
+                    {"count": {"$eq": -1}},
+                    {"count": {"$eq": -24}},
+                    {"count": {"$gt": 100}},
+                ]
+            },
+            {"count": {"$not": {"$gt": 150}}},
+            {
+                "$nor": [
+                    {"animal": {"$eq": "sheep"}},
+                    {"animal": {"$exists": False}},
+                    {"age": {"$gt": 5}},
+                    {"age": {"$lt": 10}},
+                    {"some_dict": {"k": "v"}},
+                ]
+            },
+        ],
+        "data": {"some_bool": True},
+    }
+    expected_predicates = [
+        LogicalPredicate(op="$and", field=None, mapping=mapping["$and"]),
+        ComparisonPredicate(op="$eq", field="data", target_value={"some_bool": True}),
+    ]
+
+    results = build_predicates(mapping)
+    assert results == expected_predicates
+
+    # Verify the $and operator's conditions
+    and_predicate = results[0]
+    assert len(and_predicate._conditions) == 4  # type: ignore
+
+    # First condition: {"count": {"$exists": True}}
+    assert and_predicate._conditions[0] == DataTypePredicate(  # type: ignore
+        op="$exists", field="count", target_value=True
+    )
+
+    # Second condition: $or operator
+    or_predicate = and_predicate._conditions[1]  # type: ignore
+    assert isinstance(or_predicate, LogicalPredicate)
+    assert or_predicate._op == "$or"
+    assert len(or_predicate._conditions) == 4
+    assert or_predicate._conditions == [
+        ComparisonPredicate(op="$in", field="count", target_value=[11, 17, 81]),
+        ComparisonPredicate(op="$eq", field="count", target_value=-1),
+        ComparisonPredicate(op="$eq", field="count", target_value=-24),
+        ComparisonPredicate(op="$gt", field="count", target_value=100),
+    ]
+
+    # Third condition: $not operator
+    not_predicate = and_predicate._conditions[2]  # type: ignore
+    assert isinstance(not_predicate, LogicalPredicate)
+    assert not_predicate._op == "$not"
+    assert not_predicate._conditions == [
+        ComparisonPredicate(op="$gt", field="count", target_value=150)
+    ]
+
+    # Fourth condition: $nor operator
+    nor_predicate = and_predicate._conditions[3]  # type: ignore
+    assert isinstance(nor_predicate, LogicalPredicate)
+    assert nor_predicate._op == "$nor"
+    assert len(nor_predicate._conditions) == 5
+    assert nor_predicate._conditions == [
+        ComparisonPredicate(op="$eq", field="animal", target_value="sheep"),
+        DataTypePredicate(op="$exists", field="animal", target_value=False),
+        ComparisonPredicate(op="$gt", field="age", target_value=5),
+        ComparisonPredicate(op="$lt", field="age", target_value=10),
+        ComparisonPredicate(op="$eq", field="some_dict", target_value={"k": "v"}),
+    ]
+
+
+async def test_fields_with_dollar_sign():
+    """Verify that using a field name that starts with a dollar sign produces an error."""
+    mapping = {"$bogus": 8}
+    with pytest.raises(MQLError):
+        _ = build_predicates(mapping)
